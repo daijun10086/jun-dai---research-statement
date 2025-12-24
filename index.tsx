f1b3e8ded621f1d20970bdc3c4692be86eaceadb
daijun <17612289359@163.com>
2025-12-25 01:49:56 +0800

    update

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// 如果有 index.css 文件，取消下面的注释
+// import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
